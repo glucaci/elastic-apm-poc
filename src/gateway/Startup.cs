056e23c1f31b6d8b463a60a2177f6284e938c4b4
glucaci <gabriel.lucaci.dev@gmail.com>
2021-02-16 11:56:14 +0100

    Startup with Tye

--- src/gateway/Startup.cs
+++ src/gateway/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using HotChocolate.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,19 +10,23 @@ namespace Demo.Gateway
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         public const string Accounts = "accounts";
         public const string Inventory = "inventory";
         public const string Products = "products";
         public const string Reviews = "reviews";
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient(Accounts, c => c.BaseAddress = new Uri("http://localhost:5051/graphql"));
-            services.AddHttpClient(Inventory, c => c.BaseAddress = new Uri("http://localhost:5052/graphql"));
-            services.AddHttpClient(Products, c => c.BaseAddress = new Uri("http://localhost:5053/graphql"));
-            services.AddHttpClient(Reviews, c => c.BaseAddress = new Uri("http://localhost:5054/graphql"));
+            services.AddHttpClient(Accounts, c => c.BaseAddress = CreateUri(Accounts));
+            services.AddHttpClient(Inventory, c => c.BaseAddress = CreateUri(Inventory));
+            services.AddHttpClient(Products, c => c.BaseAddress = CreateUri(Products));
+            services.AddHttpClient(Reviews, c => c.BaseAddress = CreateUri(Reviews));
 
             services
                 .AddGraphQLServer()
@@ -33,7 +38,11 @@ namespace Demo.Gateway
                 .AddTypeExtensionsFromFile("./Stitching.graphql");
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        private Uri CreateUri(string service)
+        {
+            return new Uri(Configuration.GetServiceUri(service)!, "graphql");
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
